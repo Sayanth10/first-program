193db9d79eb497480c06f884dd6be2cca7ed3f89
Sayan <sayanth10@gmail.com>
2018-02-13 21:50:19 +0530

    Implementing a scanner
    scanner to input the name

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 class Hello{
   public static void main(String[] args){
     System.out.println("Hello world");
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+    String name = input.nextLine();
+    System.out.println("Hi "+ name+" Welcome to GitHub"");
   }
 }
